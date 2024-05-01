df25de5ea2dbd8b1cacb5bc51552d3b1584cb914
vatsal <vso.opash@gmail.com>
2024-05-01 14:55:48 +0530

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://organic-api.opash.in/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://organic-api.opash.in/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+};
