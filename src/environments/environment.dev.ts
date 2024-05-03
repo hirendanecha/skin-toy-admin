e6e93223387a8ac7bf30f98243cd719273105afb
vatsal <vso.opash@gmail.com>
2024-05-03 16:10:45 +0530

    Change URLs and images

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: 'https://dev-api.organic.dating/api/v1/',
+  serverUrl: 'https://dev-api.skin.toys/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://dev-api.organic.dating/',
+  socketUrl: 'https://dev-api.skin.toys/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'https://api.organic.dating/api/v1/',
+  serverUrl: 'https://api.skin.toys/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.organic.dating/',
+  socketUrl: 'https://api.skin.toys/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 
