e12c3f1a54813a69bd3b74305f9db1e6b3df63c7
ppa-opash <ppa.opash@gmail.com>
2024-07-01 18:58:45 +0530

    Change image upload path and user details

--- src/app/@shared/components/edit-channel/edit-channel.component.ts
+++ src/app/@shared/components/edit-channel/edit-channel.component.ts
@@ -22,6 +22,7 @@ export class EditChannelComponent implements OnInit, AfterViewInit {
   channelDetails: any = {};
   memberDetails: any = {};
   selectedItems = [];
+  selectedusers = [];
   communityId: any;
   channelId: any;
   isPage = false;
@@ -33,6 +34,11 @@ export class EditChannelComponent implements OnInit, AfterViewInit {
   adminList: any;
 
   isEdit = false;
+  selectedFile: any;
+  channelImg: any = {
+    file: null,
+    url: '',
+  };
 
   constructor(
     private channelService: ChannelService,
@@ -51,6 +57,42 @@ export class EditChannelComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit(): void {
     this.getUserList();
+  }
+
+  onFileSelected(event: any) {
+    this.channelImg.file = event.target?.files?.[0];
+    this.selectedFile = URL.createObjectURL(event.target.files[0]);
+  }
+
+  removePostSelectedFile(): void {
+    this.selectedFile = null;
+  }
+
+  upload() {
+    if (this.channelImg.file) {
+      this.spinner.show();
+      this.channelService.upload(this.channelImg.file).subscribe({
+        next: (res: any) => {
+          this.spinner.hide();
+          if (this.channelImg.file?.size < 5120000) {
+            if (res.body) {
+              this.channelDetails.profile_pic_name = res?.body?.url;
+              this.saveChanges();
+            }
+          }
+        },
+        error: (err) => {
+          this.spinner.hide();
+          this.channelImg = {
+            file: null,
+            url: '',
+          };
+          return 'Could not upload the file:' + this.channelImg.file.name;
+        },
+      });
+    } else {
+      this.saveChanges();
+    }
   }
 
   getUserDetails(): void {
@@ -77,7 +119,57 @@ export class EditChannelComponent implements OnInit, AfterViewInit {
     this.isEdit = true;
   }
 
+  slugify = (str: string) => {
+    return str?.length > 0
+      ? str
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s-]/g, '')
+          .replace(/[\s_-]+/g, '-')
+          .replace(/^-+|-+$/g, '')
+      : '';
+  };
+
+  onChannelNameChange(): void {
+    if (!this.isEdit) {
+      this.isEdit = true;
+    }
+    this.channelDetails.unique_link = this.slugify(
+      this.channelDetails.firstname
+    );
+  }
+
+  onChangeTag(event: any) {
+    if (!this.isEdit) {
+      this.isEdit = true;
+    }
+    this.channelDetails.Username = event.target.value
+      .replace(/\s+/g, '')
+      .replace(/,+/g, ',');
+  }
+
   saveChanges(): void {
+    const id = this.channelDetails.id;
+    const upadtedChannelData = {
+      profileid: this.channelDetails.profileid,
+      profile_pic_name: this.channelDetails.profile_pic_name,
+      firstname: this.channelDetails.firstname,
+      Username: this.channelDetails.Username,
+      unique_link: this.channelDetails.unique_link,
+      feature: this.channelDetails.feature,
+    };
+    this.channelService.editChannal(id, upadtedChannelData).subscribe({
+      next: (res: any) => {
+        if (res) {
+          this.getUserDetails();
+          this.removePostSelectedFile();
+          if (this.isEdit) {
+            this.isEdit = false;
+          }
+        }
+      },
+    });
+
     if (this.selectedItems.length) {
       this.selectedItems.forEach((e) => {
         this.createAdmin(e);
@@ -103,6 +195,7 @@ export class EditChannelComponent implements OnInit, AfterViewInit {
       },
       error: (error) => {
         console.log(error);
+        this.toastService.danger(error.error.message);
       },
     });
   }
@@ -128,6 +221,16 @@ export class EditChannelComponent implements OnInit, AfterViewInit {
 
   onChangeData(): void {
     this.isEdit = true;
+  }
+
+  resetSelect(event) {
+    if (event?.value?.Id) {
+      this.selectedItems = this.selectedItems.filter(
+        (item) => item !== event.value.Id
+      );
+    } else {
+      this.selectedItems = [];
+    }
   }
 
   onSelectUser(item): void {
